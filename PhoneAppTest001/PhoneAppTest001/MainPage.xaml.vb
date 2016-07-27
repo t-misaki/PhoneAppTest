6682703a26d70f44258ca9418108ae722a3ba677
御前達哉 <misaki.tatsuya@nichiwa-system.co.jp>
2016-07-28 00:07:46 +0900

    コミットテスト 16.07.28

---
+++ PhoneAppTest001/PhoneAppTest001/MainPage.xaml.vb
@@ -0,0 +1,9 @@
+﻿' 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+''' <summary>
+''' それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+End Class
